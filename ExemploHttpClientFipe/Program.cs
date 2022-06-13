ccd073ff9e419fc1b9e5ebf474496c375525e677
universoti <64614511+universoti@users.noreply.github.com>
2022-06-13 20:27:06 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploHttpClientFipe/Program.cs
@@ -0,0 +1,28 @@
+﻿//Você pode alterar na url "carros" para "motos" ou "caminhoes" de acordo com a sua necessidade.
+using ExemploHttpClientFipe;
+using System.Text.Json;
+
+Console.WriteLine("Digite o tipo de veiculo!(carros -motos-caminhoes)");
+var TipoVeiculo=Console.ReadLine();
+var endereco = $@"https://parallelum.com.br/fipe/api/v1/{TipoVeiculo}/marcas";
+var client =new HttpClient();
+try
+{
+    HttpResponseMessage? response=await client.GetAsync(endereco);
+    response.EnsureSuccessStatusCode();
+    string responseBody=await response.Content.ReadAsStringAsync();
+    //Console.WriteLine(responseBody);
+    List<veiculo> Veiculos=JsonSerializer.Deserialize<List<veiculo>>(responseBody);
+
+    foreach (var veiculo in Veiculos)
+    {
+        Console.WriteLine(veiculo.nome);
+    }
+
+
+}
+catch (Exception)
+{
+
+    throw;
+}
---
+++ ExemploHttpClientFipe/veiculo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploHttpClientFipe
+{
+    public class veiculo
+    {
+        public string codigo { get; set; }
+        public string nome { get; set; }
+    }
+}
